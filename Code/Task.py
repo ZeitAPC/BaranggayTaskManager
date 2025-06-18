f7ed633a905b224ffd240ea374c8d3b1d3bf0059
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-06-18 23:34:14 +0800

    Update Task.py

--- Code/Task.py
+++ Code/Task.py
@@ -1,6 +1,6 @@
 class Task:
-    def __init__(self, taskID, task, votes = 0):
-        self.taskID = taskID    # Sets value for the task ID
-        self.task = task        # Sets value for the task name
-        self.votes = votes      # Sets value for the vote number
+    def __init__(self, taskID, taskName, votes = 0):
+        self.taskID = taskID            # Sets value for the task ID
+        self.taskName = taskName        # Sets value for the task name
+        self.votes = votes              # Sets value for the vote number
 
